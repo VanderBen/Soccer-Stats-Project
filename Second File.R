01e566f920c9b52a53d0a507aedd138b0661cbe0
Ben VanderWall <bv49@evansville.edu>
2021-09-01 14:45:42 -0600

    First experimentation with StatsBomb

--- Second File.R
+++ Second File.R
@@ -1,1 +1,23 @@
-This is my second file, more and more numbers. Blah blah blah
+library(tidyverse)
+library(StatsBombR)
+Comp <- FreeCompetitions() %>%
+  filter(competition_id==11 & season_name=="2005/2006")
+Matches <- FreeMatches(Comp)
+StatsBombData <- StatsBombFreeEvents(MatchesDF = Matches, Parallel = T)
+StatsBombData = allclean(StatsBombData)
+
+
+shots_goals = StatsBombData %>%
+  group_by(team.name) %>%
+  summarise(shots = sum(type.name=="Shot", na.rm = TRUE),
+            goals = sum(shot.outcome.name=="Goal", na.rm = TRUE))
+
+
+shots_goals = StatsBombData %>%
+  group_by(team.name) %>%
+  summarise(shots = sum(type.name=="Shot", na.rm =
+                          TRUE)/n_distinct(match_id),
+            goals = sum(shot.outcome.name=="Goal", na.rm =
+                          TRUE)/n_distinct(match_id))
+
+head(StatsBombData)
