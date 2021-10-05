debe6a84eca3e7e96c6c563be75e19ccc2b6f173
Ben VanderWall <bv49@evansville.edu>
2021-10-05 15:25:59 -0600

    - Created an archives folder for all old information: Moved old team data to it
    - Moved League Average Creator into a .R file so it can be loaded easily

---
+++ Overall/League Averager.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(readxl)
+
+drake <- read_excel("Data/Drake.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "Drake", .after = Date)
+illinois_state <- read_excel("Data/Illinois State.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "Illinois State", .after = Date)
+indiana_state <- read_excel("Data/Indiana State.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "Indiana State", .after = Date)
+loyola <- read_excel("Data/Loyola.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "Loyola", .after = Date)
+ue <- read_excel("Data/Evansville.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "UE", .after = Date)
+siu <- read_excel("Data/SIU.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "SIU", .after = Date)
+uni <- read_excel("Data/UNI.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "UNI", .after = Date)
+valpo <- read_excel("Data/Valpo.xlsx", na = "-") %>% 
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "Valpo", .after = Date)
+
+league <- bind_rows(drake, illinois_state, indiana_state, loyola, ue, siu, uni, valpo)
+league[is.na(league)] = 0
