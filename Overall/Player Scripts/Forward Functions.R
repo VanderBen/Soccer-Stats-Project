c1507113fad4f7e6e22da210852b4bf2e4841016
Ben VanderWall <bmvw19@gmail.com>
2021-11-04 21:01:00 -0700

    - Finished player functions for Valpo game - Need to fix a lot of names

--- Overall/Player Scripts/Forward Functions.R
+++ Overall/Player Scripts/Forward Functions.R
@@ -16,7 +16,47 @@ form_mean_player <- function(playing_for, section) {
   tmp
 }
 
-forward_form <- function(team, section) {
-  tmp2 <- filter(section, Team == team)
-  tmp2
+forward_form <- function(section) {
+  select(section, "Name", "Player Number", "InStat Index", "Matches played", "Minutes played", "Starting lineup appearances",
+         "Substitutes in", "Goals",  "Shots", "Shots on target", "xG (Expected goals)", "xG conversion", "Chances", "Assists",
+         "Expected assists", "Accurate passes", "Key passes", "Key passes accurate", "Ball recoveries in opponent's half",
+         "Dribbles", "Attacking challenges", "Attacking challenges won", "Ball interceptions", "Fouls suffered")
 }
+
+winger_form <- function(section) {
+  select(section, "Name", "Player Number", "InStat Index", "Matches played", "Minutes played", "Starting lineup appearances",
+         "Substitutes in", "Goals",  "Shots", "Shots on target", "xG (Expected goals)", "xG conversion", "Chances", "Assists", 
+         "Crosses", "Crosses accurate", "Expected assists", "Accurate passes", "Key passes", "Key passes accurate",
+         "Ball recoveries in opponent's half", "Dribbles", "Attacking challenges", "Attacking challenges won", "Ball interceptions",
+         "Fouls suffered")
+}
+
+midfielder_form <- function(section) {
+  select(section, "Name", "Player Number", "InStat Index", "Matches played", "Minutes played", "Starting lineup appearances",
+         "Substitutes in", "Goals",  "Shots", "xG (Expected goals)", "Chances", "Assists", "Key passes", "Key passes accurate",
+         "Expected assists", "Accurate passes", "Lost balls", "Lost balls in own half", "Attacking challenges", "Attacking challenges won", "Tackles", "Tackles successful",
+         "Defensive challenges", "Defensive challenges won", "Air challenges", "Air challenges won", "Ball interceptions", 
+         "Ball recoveries in opponent's half", "Fouls suffered")
+}
+
+fullback_form <- function(section) {
+  select(section, "Name", "Player Number", "InStat Index", "Matches played", "Minutes played", "Starting lineup appearances",
+         "Substitutes in", "Assists", "Key passes", "Key passes accurate",
+         "Accurate passes", "Lost balls", "Lost balls in own half", "Tackles", "Tackles successful",
+         "Defensive challenges", "Defensive challenges won", "Air challenges", "Air challenges won", "Ball interceptions", 
+         "Ball recoveries in opponent's half", "Fouls suffered")
+}
+
+defender_form <- function(section) {
+  select(section, "Name", "Player Number", "InStat Index", "Matches played", "Minutes played", "Starting lineup appearances",
+         "Substitutes in",
+         "Accurate passes", "Lost balls", "Lost balls in own half", "Tackles", "Tackles successful",
+         "Defensive challenges", "Defensive challenges won", "Air challenges", "Air challenges won", "Ball interceptions", 
+         "Ball recoveries in opponent's half", "Fouls suffered")
+}
+
+
+
+
+
+
