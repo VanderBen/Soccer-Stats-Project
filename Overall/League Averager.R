8401b9738a74fbf5a20376d5c5e09bc9bb3ac683
Ben VanderWall <bv49@evansville.edu>
2021-11-04 19:32:09 -0600

    Added Missouri State to the project

--- Overall/League Averager.R
+++ Overall/League Averager.R
@@ -25,6 +25,10 @@ uni <- read_excel("Data/UNI.xlsx", na = "-") %>%
 valpo <- read_excel("Data/Valpo.xlsx", na = "-") %>% 
   filter(row_number() <= n()-1) %>%
   mutate(Team = "Valpo", .after = Date)
+miss <- read_excel("Data/Miss St.xlsx", na = "-") %>%
+  filter(row_number() <= n()-1) %>%
+  mutate(Team = "Miss St", .after = Date)
+
 
 league <- bind_rows(drake, illinois_state, indiana_state, loyola, ue, siu, uni, valpo)
 league[is.na(league)] = 0
